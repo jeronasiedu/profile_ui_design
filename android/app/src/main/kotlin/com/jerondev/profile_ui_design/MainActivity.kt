62686586dbc261fe855f1f65307de8f2f82cff11
Jeron <norejudeisa@gmail.com>
2023-01-11 18:30:55 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/jerondev/profile_ui_design/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jerondev.profile_ui_design
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:profile_ui_design/root_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Profile Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: const RootApp(),
+    );
+  }
+}
